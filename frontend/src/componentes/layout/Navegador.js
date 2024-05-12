c599643dae289ea9d5d26ae39e78e8d52bcd67d5
Maximiliano Noriega <maxinoriega1983@gmail.com>
2024-05-12 10:19:05 +0200

    version final

--- frontend/src/componentes/layout/Navegador.js
+++ frontend/src/componentes/layout/Navegador.js
@@ -1,21 +1,25 @@
 import React from 'react';
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navegador = (props) => {
     return (
         <nav>
             <div className='holder'>
-
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/nosotros">Nosotros</Link></li>
-                    <li><Link to="/gastronomia">Gastronomia</Link></li>
-                    <li><Link to="/lugares">Lugares</Link></li>
-                    <li><Link to="/novedades">Novedades</Link></li>
-                    <li><Link to="/contacto">Contacto</Link></li>
+                    <li><NavLink to="/" className={({ isActive }) => isActive ? "activo" :
+                    undefined}>Home</ NavLink></li>
+                    <li><NavLink to="/nosotros" className={({ isActive }) => isActive ? "activo" :
+                    undefined}>Nosotros</ NavLink></li>
+                    <li><NavLink to="/gastronomia" className={({ isActive }) => isActive ? "activo" :
+                    undefined}>Gastronomia</ NavLink></li>
+                    <li><NavLink to="/lugares" className={({ isActive }) => isActive ? "activo" :
+                    undefined}>Lugares</ NavLink></li>
+                    <li><NavLink to="/novedades" className={({ isActive }) => isActive ? "activo" :
+                    undefined}>Novedades</ NavLink></li>
+                    <li><NavLink to="/contacto" className={({ isActive }) => isActive ? "activo" :
+                    undefined}>Contacto</ NavLink></li>
                 </ul>
-
             </div>
         </nav>
     );
